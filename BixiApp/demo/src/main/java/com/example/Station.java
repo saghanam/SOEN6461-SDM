6c2f0847f80dcf8a1bbf9b27dbe11d49edbe8917
Admin <Admin@LAPTOP-639NIPOO.home>
2023-03-28 03:15:07 -0400

    Class File 2

--- BixiApp/demo/src/main/java/com/example/Station.java
+++ BixiApp/demo/src/main/java/com/example/Station.java
@@ -1,5 +1,29 @@
 package com.example;
 
-public class Station {
+import java.util.ArrayList;
 
+public class Station {
+	private String station_code;
+	private ArrayList<Dock> docks;
+	
+	public String getStation_Code() {
+		return this.station_code;
+	}
+	
+	public ArrayList<Dock> getDocks(){
+		return this.docks;
+	}
+	
+	public void setStation_Code(String st_code) {
+		this.station_code = st_code;
+	}
+	
+	public void addDock(Dock dk) {
+		this.docks.add(dk);
+	}
+	
+	public void removeDock(Dock dk) {
+		this.docks.remove(dk);
+	}
+	
 }
